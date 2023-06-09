035ba071994ccf9288e1b3975c09da0ff9099bbe
Mahesh <86904681+Mahesh262@users.noreply.github.com>
2023-06-09 14:49:31 +0530

    Create ImageUpload.jsx

---
+++ ImageUpload.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const ImageUpload = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setSelectedImage(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" accept="image/*" onChange={handleImageUpload} />
+      {selectedImage && <img src={selectedImage} alt="Uploaded" />}
+    </div>
+  );
+};
+
+export default ImageUpload;
